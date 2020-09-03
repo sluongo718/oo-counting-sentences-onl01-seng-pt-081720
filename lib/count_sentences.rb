eec4105f983a83898a1701c138255f4d28055e31
sam l <sluongo718@yahoo.com>
2020-09-03 21:46:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   this_sentence =  self.split(/\.|\?|\!/)
+    newer_array = this_sentence.delete_if{|sentence| sentence.length < 2}
+    return newer_array.count
   end
 end
